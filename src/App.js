94d30bd18f645ffdfe6c2ae59ef1e2834cf2e803
corweb2025 <dudrira@gmail.com>
2025-07-30 01:30:35 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './component/Customer';
+
+const customers = [
+  {
+  'id' : '1',
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '710119',
+  'gender' : '남자',
+  'job' : '프로그래머'
+  },
+  {
+  'id' : '2',
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '710119',
+  'gender' : '남자',
+  'job' : '프로그래머'
+  },
+  {
+  'id' : '3',
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '710119',
+  'gender' : '남자',
+  'job' : '프로그래머'
+  },
+]
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      {
+        customers.map(c => {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
 
-        <img src={logo} className="App-logo" alt="logo" />
- 
-          Hi React !!
 
     </div>
   );
